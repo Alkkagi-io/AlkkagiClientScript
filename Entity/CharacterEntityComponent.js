6f8d998974623b393c0218250be5059853da6f82
SEH00N <06psh0501@gmail.com>
2025-11-01 16:02:22 +0900

    [Audio] Character Dead Sound

--- Entity/CharacterEntityComponent.js
+++ Entity/CharacterEntityComponent.js
@@ -1,17 +1,56 @@
 const CharacterEntityComponent = pc.createScript('characterEntityComponent');
 
 CharacterEntityComponent.prototype.initialize = function() {
+    const entityComponent = this.entity.script.entityComponent;
+    entityComponent.getEvents().on('entityInitialized', this.onEntityInitialized, this);
+    entityComponent.getEvents().on('entityUpdated', this.onEntityUpdated, this);
+
     const rigidbody = this.entity.rigidbody;
     rigidbody.on('triggerenter', this.onTriggerEnter, this);
     rigidbody.on('collisionstart', this.onCollisionStart, this);
+
+    this.isDead = false;
 };
 
+CharacterEntityComponent.prototype.onEntityInitialized = function(entityStaticData) {
+}
+
+CharacterEntityComponent.prototype.onEntityUpdated = function(elapsedMS, prevEntityDynamicData, entityDynamicData) {
+    if(this.isDead) {
+        return;
+    }
+
+    if(entityDynamicData.hpPer / 100 > 0) {
+        return;
+    }
+
+    this._die();
+}
+
 CharacterEntityComponent.prototype.onTriggerEnter = function(entity) {
+    if(this.isDead) {
+        return;
+    }
+
     const soundComponent = entity.script.entitySoundComponent;
     soundComponent?.playSound('collision');
 };
 
 CharacterEntityComponent.prototype.onCollisionStart = function(result) {
+    if(this.isDead) {
+        return;
+    }
+
     const soundComponent = result.other.script.entitySoundComponent;
     soundComponent?.playSound('collision');
 };
+
+CharacterEntityComponent.prototype._die = function() {
+    this.isDead = true;
+
+    const soundComponent = this.entity.script.entitySoundComponent;
+    soundComponent?.playSound('dead');
+
+    // 필요시 연출 추가
+    // 직접적으로 destroy를 호출해선 안 된다.
+}
