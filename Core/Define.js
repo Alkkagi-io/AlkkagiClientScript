d5c7ed216d9859b3230a23ead66f37a9ca2b2d9f
SEH00N <06psh0501@gmail.com>
2025-11-02 23:46:21 +0900

    [etc]

--- Core/Define.js
+++ Core/Define.js
@@ -10,9 +10,15 @@
         static WS_ADDRESS = 'ws://localhost:3000/ws';
         
         // dev
-        // static CDN_ADDRESS = 'https://alkkagidev.plasticpipe.tube/data';
-        // static SHARED_BUNDLE_ADDRESS = 'https://alkkagidev.plasticpipe.tube/sharedbundle';
+        // static CDN_ADDRESS = 'https://alkkagidev.plasticpipe.tube:9696/data';
+        // static SHARED_BUNDLE_ADDRESS = 'https://alkkagidev.plasticpipe.tube:9696/sharedbundle';
         // static WS_ADDRESS = 'wss://alkkagidev.plasticpipe.tube:9696/ws';
+        // static WS_ADDRESS = 'ws://localhost:3000/ws';
+        
+        // live
+        // static CDN_ADDRESS = 'https://alkkagi-io.plasticpipe.tube:5996/data';
+        // static SHARED_BUNDLE_ADDRESS = 'https://alkkagi-io.plasticpipe.tube:5996/sharedbundle';
+        // static WS_ADDRESS = 'wss://alkkagi-io.plasticpipe.tube:5996/ws';
     }
 
     root.Define = Define;
