322aa88b10f50adc3a5ff824d0a71b3087a94ac5
SEH00N <06psh0501@gmail.com>
2025-11-03 20:29:29 +0900

    [Entity] XpEntity Color Step

--- Entity/XPContainerEntityComponent.js
+++ Entity/XPContainerEntityComponent.js
@@ -1,10 +1,27 @@
 const XPContainerEntityComponent = pc.createScript('xpContainerEntityComponent');
 
+XPContainerEntityComponent.attributes.add('innerImage', {
+    type: 'entity'
+});
+
+XPContainerEntityComponent.attributes.add('outlineImage', {
+    type: 'entity'
+});
+
+XPContainerEntityComponent.attributes.add('innerColorList', {
+    type: 'rgb',
+    array: true
+});
+
+XPContainerEntityComponent.attributes.add('outlineColorList', {
+    type: 'rgb',
+    array: true
+});
+
 XPContainerEntityComponent.prototype.initialize = function() {
     const entityComponent = this.entity.script.entityComponent;
     entityComponent.getEvents().on('entityInitialized', this.onEntityInitialized, this);
 };
 
 XPContainerEntityComponent.prototype.onEntityInitialized = function(entityStaticData) {
-    console.log(`XPContainerEntityComponent: onEntityInitialized, xpUnit: ${entityStaticData.xpUnit}`);
 };
--- Entity/XPObjectEntityComponent.js
+++ Entity/XPObjectEntityComponent.js
@@ -1,10 +1,27 @@
 const XPObjectEntityComponent = pc.createScript('xpObjectEntityComponent');
 
+XPObjectEntityComponent.attributes.add('innerImage', {
+    type: 'entity'
+});
+
+XPObjectEntityComponent.attributes.add('outlineImage', {
+    type: 'entity'
+});
+
+XPObjectEntityComponent.attributes.add('innerColorList', {
+    type: 'rgb',
+    array: true
+});
+
+XPObjectEntityComponent.attributes.add('outlineColorList', {
+    type: 'rgb',
+    array: true
+});
+
 XPObjectEntityComponent.prototype.initialize = function() {
     const entityComponent = this.entity.script.entityComponent;
     entityComponent.getEvents().on('entityInitialized', this.onEntityInitialized, this);
 };
 
 XPObjectEntityComponent.prototype.onEntityInitialized = function(entityStaticData) {
-    console.log(`XPObjectEntityComponent: onEntityInitialized, xpAmount: ${entityStaticData.xpAmount}, lifeTime: ${entityStaticData.lifeTime}`);
 };
