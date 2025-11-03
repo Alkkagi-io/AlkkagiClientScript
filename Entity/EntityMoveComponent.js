6cb565b39de23d7af02a11a648d9f99d1fdf67e5
SEH00N <06psh0501@gmail.com>
2025-11-03 22:00:01 +0900

    [Entity] Positioning Interpolate

--- Entity/EntityMoveComponent.js
+++ Entity/EntityMoveComponent.js
@@ -1,9 +1,20 @@
 const EntityMoveComponent = pc.createScript('entityMoveComponent');
 
 const MOVE_THRESHOLD = 1;
+// const JITTER_WEIGHT = 1;
+// const JITTER_ALLOWANCE_MS = 200;
+// const MIN_DT = 0.01;
+// const MAX_DT = 0.3;
+// const VELOCITY_BLEND = 0.6;
+// const POSITION_CORRECTION_STRENGTH = 0.3;
 
 EntityMoveComponent.prototype.initialize = function() {
+    this.lastReceivedTime = 0;
+    // this.prevElapsedErrorMS = 0;
     this._velocity = new pc.Vec3();
+    this._targetVelocity = new pc.Vec3();
+    this._prevTargetPositionBuffer = new pc.Vec3();
+    this._currentTargetPositionBuffer = new pc.Vec3();
 
     const entityComponent = this.entity.script.entityComponent;
     entityComponent.getEvents().on('entityInitialized', this.onEntityInitialized, this);
@@ -11,29 +22,52 @@ EntityMoveComponent.prototype.initialize = function() {
 };
 
 EntityMoveComponent.prototype.update = function(dt) {
+    // this._velocity.lerp(this._velocity, this._targetVelocity, VELOCITY_BLEND * dt);
     this.entity.translate(this._velocity.x * dt, this._velocity.y * dt, this._velocity.z * dt);
 };
 
 EntityMoveComponent.prototype.onEntityInitialized = function(entityStaticData) {
     this._velocity.set(0, 0, 0);
-    this.entity.setPosition(this._to3D(entityStaticData.position));
+    // this._targetVelocity.set(0, 0, 0);
+    this.lastReceivedTime = Date.now();
+    this._to3D(entityStaticData.position, this._prevTargetPositionBuffer);
+    this._to3D(entityStaticData.position, this._currentTargetPositionBuffer);
+    this.entity.setPosition(this._prevTargetPositionBuffer);
 };
 
 EntityMoveComponent.prototype.onEntityUpdated = function(elapsedMS, prevEntityDynamicData, entityDynamicData) {
-    const prevTargetPosition = prevEntityDynamicData == null ? this.entity.getPosition() : this._to3D(prevEntityDynamicData.position);
-    const currentTargetPosition = this._to3D(entityDynamicData.position);
+    const currentPosition = this.entity.getPosition();
+    const currentTime = Date.now();
 
-    if(prevTargetPosition.distance(this.entity.getPosition()) > MOVE_THRESHOLD) {
+    // const rawElapsedErrorMS = elapsedMS - (currentTime - this.lastReceivedTime);
+    // const clampedElapsedErrorMS = Math.max(Math.min(rawElapsedErrorMS, JITTER_ALLOWANCE_MS), -JITTER_ALLOWANCE_MS);
+    // const elapsedErrorMS = this.prevElapsedErrorMS * (1 - JITTER_WEIGHT) + clampedElapsedErrorMS * JITTER_WEIGHT;
+
+    const elapsedErrorMS = elapsedMS - (currentTime - this.lastReceivedTime);
+    // this.prevElapsedErrorMS = elapsedErrorMS;
+    
+    this.lastReceivedTime = currentTime;
+
+    const prevTargetPosition = prevEntityDynamicData == null ? currentPosition : this._to3D(prevEntityDynamicData.position, this._prevTargetPositionBuffer);
+    const currentTargetPosition = this._to3D(entityDynamicData.position, this._currentTargetPositionBuffer);
+
+    if(prevTargetPosition.distance(currentPosition) > MOVE_THRESHOLD) {
+        // currentPosition.lerp(currentPosition, prevTargetPosition, POSITION_CORRECTION_STRENGTH);
         this.entity.setPosition(prevTargetPosition);
+        // this._velocity.set(0, 0, 0);
+        // this._targetVelocity.set(0, 0, 0);
+        // return;
     }
 
-    const dt = Math.max(elapsedMS * 0.001, 1e-6);
-    const currentPosition = this.entity.getPosition();
-    this._velocity.sub2(currentTargetPosition, currentPosition).scale(1 / dt);
+    // const dt = Math.max(Math.min((elapsedMS + elapsedErrorMS) * 0.001, MAX_DT), MIN_DT);
+    const dt = (elapsedMS + elapsedErrorMS) * 0.001;
+    // this._targetVelocity.sub2(currentTargetPosition, this.entity.getPosition()).scale(1 / dt);
+    this._velocity.sub2(currentTargetPosition, this.entity.getPosition()).scale(1 / dt);
 };
 
-EntityMoveComponent.prototype._to3D = function (p2) {
-    return new pc.Vec3(p2.x, p2.y, 0);
+EntityMoveComponent.prototype._to3D = function (p2, out) {
+    out.set(p2.x, p2.y, 0);
+    return out;
 };
 
 EntityMoveComponent.prototype.getVelocity = function() {
